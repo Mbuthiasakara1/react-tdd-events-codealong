d99f0c7cd5becdd75089f680418b7c4c5349fc62
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-08-09 14:36:33 +0300

    testing library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 function App() {
+  const [pepperoniIsChecked, setpepperoniIsChecked] = useState(false);
+
+  function togglePepperoni(e) {
+    setpepperoniIsChecked(e.target.checked);
+  }
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input
+        onChange={togglePepperoni}
+        type="checkbox"
+        id="pepperoni"
+        checked={pepperoniIsChecked}
+        aria-checked={pepperoniIsChecked}
+      />
+      <label htmlFor="pepperoni">Add Pepperoni</label>
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepperoniIsChecked ? <li>Pepperoni</li> :null}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,37 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 
-import '@testing-library/jest-dom';
 
 // Code tests here
+test("pizza checkbox is initially unchecked", () => {
+  render(<App />);
+  const addPepperoni = screen.getByRole("checkbox", {
+    name: /add pepperoni/i,
+  });
+  expect(addPepperoni).not.toBeChecked();
+});
+
+test("pizaa list initially contains only cheese", () => {
+  render(<App />);
+  const listItems = screen.getAllByRole("listitem");
+  expect(listItems.length).toBe(1);
+
+  const myitem = screen.getByText("Cheese");
+  expect(myitem).toBeInTheDocument();
+
+  const myitem2 = screen.queryByText("Pepperoni");
+  expect(myitem2).not.toBeInTheDocument();
+});
+//effect of clicking the checkbox
+test("checkbox become checked when user clicks it", () => {
+  render(<App />);
+  const addPepperoni = screen.getByRole("checkbox", {
+    name: /add pepperoni/i,
+  });
+  userEvent.click(addPepperoni);
+  expect(addPepperoni).toBeChecked();
+});
+
+test("topping appears in toppings list when checked", () => {});
